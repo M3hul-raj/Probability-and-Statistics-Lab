65df512468f9d8c61367214ced994cd57195811a
M3hul-raj <143120371+M3hul-raj@users.noreply.github.com>
2024-08-29 22:57:05 +0530

    Update Day1Prg1.cpp

--- Day1Prg1.cpp
+++ Day1Prg1.cpp
@@ -6,19 +6,15 @@ int main()
     int arr[] = {38,40,47,44,46,43,49,53};
     int m = 0;
     cout<<"Mean is : ";
-    for(int i=0; i<8; i++){
-        m = arr[i] + m;
-    }
+    for(int i=0; i<8; i++) m = arr[i] + m;
     float mean = (float)m/8;
     cout<<(mean);
 
     cout<<"\nVariance is : ";
     float v = 0;
-    for(int j=0; j<8; j++){
-        v = (mean-arr[j])*(mean-arr[j]) + v;
-    }
+    for(int j=0; j<8; j++) v = (mean-arr[j])*(mean-arr[j]) + v;
     float var = v/8;
     cout<<var;
-
+    
     cout<<"\nCoefficient of Variation is : "<<(var/mean);
 }
