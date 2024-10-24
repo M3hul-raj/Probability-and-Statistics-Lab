870bba70e56301c345d3e00deb82c7a1254ff659
M3hul-raj <143120371+M3hul-raj@users.noreply.github.com>
2024-10-25 02:56:36 +0530

    Update day_4_prg_1_and_2_.cpp

--- day_4_prg_1_and_2_.cpp
+++ day_4_prg_1_and_2_.cpp
@@ -1,7 +1,13 @@
-/* Draw a scatter plot for the given bivariate data and find the correlation coefficient between 𝑥 and 𝑦 for the data in the dosage example:
+/* 1.  Draw a scatter plot for the given bivariate data and find the correlation coefficient between 𝑥 and 𝑦 for the data in the dosage example:
    Dosage (x)               | 3  3  4  5  6  6  7  8  8  9
    Duration of relief (y)   | 9  5  12 9  14 16 22 18 24 22  
-   Draw a scatter plot for the given bivariate data.    */
+   Draw a scatter plot for the given bivariate data.    
+
+2. Soil temperature (x_i, in °C) and germination interval (y_i, in days) were observed for 
+   winter wheat at 10 localities 
+   x_i    | 12.5   5.0   3.0   5.0   6.5   6.0   4.0   7.0   5.5   4.0
+   y_i    | 10     26    41    29    27    19    18    20    28    33 
+   Draw a scatter plot for the given bivariate data. Find the correlation coefficient between x_i and y_i.   */
 
 #include <iostream>
 #include <math.h>
