63e2425d0473b5d75063bfc8af91a1d96a22df44
M3hul-raj <143120371+M3hul-raj@users.noreply.github.com>
2024-10-25 02:28:23 +0530

    Update day_7_prg1_and_2_.cpp

--- day_7_prg1_and_2_.cpp
+++ day_7_prg1_and_2_.cpp
@@ -1,3 +1,25 @@
+/* **Question 1**
+
+A random sample of 20 nominally measured 2mm diameter steel ball bearings is taken and the diameters are measured precisely. The measurements, in mm, are as follows:
+
+2.02  1.94  2.09  1.95  1.98  2.00  2.03  2.04  2.08  2.07
+1.99  1.96  1.99  1.95  1.99  1.99  2.03  2.05  2.01  2.03
+
+Assuming that the diameters are normally distributed with unknown mean, µ, and unknown variance σ²,
+
+(a) find a two-sided 95% confidence interval for the variance, σ²;                                                                      
+(b) find a two-sided confidence interval for the standard deviation, σ.
+
+**Question 2**
+
+Measurements are made on the lengths, in mm, of a sample of twenty wooden components for self-assembly furniture. Assume that these may be regarded as twenty independent observations from a normal distribution with unknown mean µ and unknown variance σ². The data are as follows.
+
+581  580  581  577  580  581  577  579  579  578
+581  583  577  578  582  581  582  580  582  579
+
+Find a 95% confidence interval for the variance σ² and hence find a 95% confidence interval for the standard deviation σ.
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
